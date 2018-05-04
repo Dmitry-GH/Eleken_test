641704890cdc69e3fd5bb959bbbfc412f8596dbc
Dmitry Rudiuk <drakonu41419@gmail.com>
2018-05-05 01:43:48 +0300

    card view && mobile

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 Eleken_test
 
 Url: https://dmitry-gh.github.io/Eleken_test/
+
+Use npm start - for build dev version and run local server
+Use npm build:prod - for build prod version
+Use npm watch - for watching file changes
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,20 @@
-console.log('test');
+let table_view_btn = document.getElementById("table_view");
+let card_view_btn = document.getElementById("card_view");
+let view_target =  document.getElementById("view_target");
+
+table_view_btn.addEventListener("click", toggleView);
+card_view_btn.addEventListener("click", toggleView);
+
+function toggleView() {
+    
+    if (this.id === 'table_view') {
+        
+        view_target.classList.remove('card_view');
+        this.style.opacity = "1";
+        card_view_btn.style.opacity = "0.5";
+    } else {
+        view_target.classList.add('card_view');
+        this.style.opacity = "1";
+        table_view_btn.style.opacity = "0.5";
+    }
+}
